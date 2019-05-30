7ddf5012170439fb91800dd1ecbfb37f7feff63d
Regis Ribeiro <regisgomesr@gmail.com>
2019-05-29 22:07:07 -0300

    Fixing public.

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ const dbConnection = sqlite.open(path.resolve(__dirname, 'banco.sqlite'), { Prom
 
 const bodyParser = require('body-parser')
 
-const port = process.env.port || 3000
+const port = process.env.PORT || 3000
 
 const path = require('path')
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.use(bodyParser.urlencoded({ extended: true }))
 
